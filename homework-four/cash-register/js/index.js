c20710a89ead77e01894db5d08980d2a1744475b
peylee <peytonnlee@gmail.com>
2016-03-24 18:23:58 -0500

    final project lol

--- homework-four/cash-register/js/index.js
+++ homework-four/cash-register/js/index.js
@@ -1,48 +1,59 @@
-// Create a global variable for the total
+// Declare global variable for the total
 
 var total = 0;
 
-// Create a function you can run when you submit the field Hint: .submit() in jQuery
+// Tie event to happen when you hit enter - give it the function that we want it to run
 
 $("#entry").submit(enter);
 
-// // Remember parseFloat() and toFixed() to deal with formatting numbers. 
-//This will come in handy when dealing with displaying a "currency friendly" format 
-// //(HINT: create an additional function to format your number into a "currency friendly" format)
-// 	function currencyFormat(number) {
+
 
 function enter() {
-
-	var entry;
-
-	event.preventDefault();
+//getting the value of the input with an id of #newEntry
 
 	var entry = $("#newEntry").val();
+
+// taking the string of the text in the input and turning it into a number so that we can work with it and change it in JS
 	var entry = parseFloat(entry);
 
-	$("#entries").append("<tr><td>&dollar;" + entry + "<tr><td>");
+//declaring a variable of 'currency' and running the currencyFormat() function. notice 'entry' is being passed into this function
+//currency should equal '$20.00'
+	var currency = currencyFormat(entry);
 
-	total = entry + total;
+//append a new table row with 'currency' ($20.00) inside of it 
+	$("#entries").append("<tr><td></td><td>" + currency + "</td><tr>");
 
-	$("#total").html(total);
+//keeping a tally of what has been added so far
+	total += entry;
 
+//get it to display the total
+	$("#total").html(currencyFormat(total));
+
+//clear the field for the next value. val with an empty string in it, field is empty every time the function is ran
 	$('#newEntry').val('');
 
-
+//housekeeping thing - not going to return anything, function does it's job
+	return false;
 };
 
 
-function currencyFormat() {
-	var currency;
+function currencyFormat(number) {
 
-	var currency = parseFloat(entry);
+	var currency = parseFloat(number);
+
+//format the user's input to '20.00' if they put in '20'
 	currency = toFixed(2);
 
+//taking the number and adding a $ in front of it
+	currency = "$" + currency;
+
+// this should return '$20.00' if '20' was entered 
 	return currency;
+
+//outside of here, you do not have access to the currency variable
+
 }
 
-
-// ugh this is as far as i got :( 
 
 
 
